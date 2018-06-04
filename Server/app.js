f06ffe786bb155b0be1c252720d212b0ad31275c
Rohit <rohit.galgali@gmail.com>
2018-06-04 13:51:45 +0530

    Added routing. No need to start two servers

--- Server/app.js
+++ Server/app.js
@@ -1,24 +1,25 @@
 var express = require('express');
 var app = express();
 
+var path = require('path');
 
 var SerialPort = require('serialport');
-var port = new SerialPort('COM6', {
-  baudRate: 9600
-})
+// var port = new SerialPort('COM6', {
+//   baudRate: 9600
+// })
 
 app.get('/LightOn', function(req, res){
-  port.write('H');
+  //port.write('H');
   res.send("LightOn");
 });
 
 app.get('/LightOff', function(req, res){
-  port.write('L');
+ // port.write('L');
   res.send("LightOff");
 });
 
 app.get('/', function(req, res){
-  res.sendFile("../Client/LightControl.html");
+  res.sendFile(path.join(__dirname, '../Client', 'LightControl.html'));
 });
 
 app.listen(3000);
